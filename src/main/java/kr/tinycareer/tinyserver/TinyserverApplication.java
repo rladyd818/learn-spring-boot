6b9fcf2922a62b6dcce87479d5a59cad7de7eeb7
Kim Yong <rladyd818@live.co.kr>
2024-06-14 20:43:57 +0900

    feat: add initialized spring boot project

---
+++ src/main/java/kr/tinycareer/tinyserver/TinyserverApplication.java
@@ -0,0 +1,13 @@
+package kr.tinycareer.tinyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/tinycareer/tinyserver/TinyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.tinycareer.tinyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
