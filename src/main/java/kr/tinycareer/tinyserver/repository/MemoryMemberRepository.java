94025b05f0c6e1447b7cbbfc6dc07595f65d2012
Kim Yong <rladyd818@live.co.kr>
2024-06-16 11:13:30 +0900

    feat: add 김영한님 스프링 입문 9

---
+++ src/main/java/kr/tinycareer/tinyserver/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package kr.tinycareer.tinyserver.repository;
+
+import kr.tinycareer.tinyserver.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // store와 sequence 둘 다 동시성 문제 발생할 수 있음. 원래는 동시성 문제 해결을 위한 별도 선언이 필요함.
+    private Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
